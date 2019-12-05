3034191ea157282e8426a3e0e11fcbaf157cbcbe
AConanDoyle <H>
2019-12-05 12:14:17 +0100

    init commit

--- js/app.js
+++ js/app.js
@@ -1,24 +1,36 @@
 // initialize our map
+var access_token = 'access_token=pk.eyJ1IjoiYW5kcm9pZHBpdGkiLCJhIjoiY2lvd3VwamhmMDA4MHZ0a2p0OGJnYnRhNSJ9.fITbznDvS6FhARaYxrW_Pw#10';
 var map = L.map('map', {
-    center:[52.5200066, 13.404954],
-    zoom:12
+    'center': [52.5200066, 13.404954],
+    'zoom': 12,
+    'layers': [
+        L.tileLayer('https://api.mapbox.com/styles/v1/androidpiti/ck2j9fns70ul51cpn8rqnmqb1/tiles/256/{z}/{x}/{y}?' + access_token, {
+            'attribution': '© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        })
+    ]
 });
 
-//add openstreet baselayer to the map 
-var MapBox = L.tileLayer("https://api.mapbox.com/styles/v1/androidpiti/ck2j9fns70ul51cpn8rqnmqb1.html?fresh=true&title=true&access_token=pk.eyJ1IjoiYW5kcm9pZHBpdGkiLCJhIjoiY2lvd3VwamhmMDA4MHZ0a2p0OGJnYnRhNSJ9.fITbznDvS6FhARaYxrW_Pw#10.0/42.362400/-71.020000/0", {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// custom icon
+var gloveIcon = L.icon({
+    iconSize: [28, 45],
+    iconAnchor: [12, 41],
+    popupAnchor: [-6, -41],
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png'
+});
+
+// set pop up windows
+function onEachFeature(feature, layer) {
+    layer.bindPopup("<img alt='image glove' text-align='justify' width='100px' src='" +
+        layer.feature.properties.imageurl + "'/> <br>" + layer.feature.properties.name + "<br>" + layer.feature.properties.description, { 'className': 'custom' });
+}
+
+// add data to map
+L.geoJson(gloves, {
+    pointToLayer: function(feature, latlng) {
+        return L.marker(latlng, {
+            icon: gloveIcon
+        });
+    },
+    onEachFeature: onEachFeature
 }).addTo(map);
-
-
-// define a blank geoJSON Layer
-var gloves = L.geoJSON(null);
-
-//get the geojson data with ajax, and add it to the blank layer we created
-$.getJSON('data/gloves.geojson',function(data){
-	gloves.addData(data);
-	map.fitBounds(gloves.getBounds());
-});
-
-// finally add the layer to the map
-gloves.addTo(map);
